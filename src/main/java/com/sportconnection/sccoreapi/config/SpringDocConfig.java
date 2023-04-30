28f42c2cd469d08bde17fb743d174335ff5bf28e
lalakagawa <lalakagawa@hotmail.com>
2023-04-30 14:28:50 -0300

    Ajuste na config do swagger para aceitar autenticação

--- src/main/java/com/sportconnection/sccoreapi/config/SpringDocConfig.java
+++ src/main/java/com/sportconnection/sccoreapi/config/SpringDocConfig.java
@@ -1,20 +1,28 @@
 package com.sportconnection.sccoreapi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 public class SpringDocConfig {
-
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().info(new Info()
-                .title("Sport Connection - Core API")
-                .version("1.0.0")
-        );
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(Arrays.asList(new SecurityRequirement().addList("BearerAuth")))
+                .info(new Info().title("Sport Connection - Core API").version("1.0.0"));
     }
 
     @Bean
@@ -24,5 +32,6 @@ public class SpringDocConfig {
                 .pathsToMatch("/**")
                 .build();
     }
+
 
 }
